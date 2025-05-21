9837ec1478d5388d9222a1982a6612ace842cca7
ameliaBuggy <amelia.chiasson@gmail.com>
2025-05-21 09:35:55 -0400

    WilliamHater class added

--- TestRunner.java
+++ TestRunner.java
@@ -3,6 +3,12 @@ public class TestRunner {
         Hater katie = new Hater("Kehinde", true);
 
         katie.addReason("He has a dumb grin on his face");
-        
+        katie.printReasons();
+
+        WilliamHater katie2 = new WilliamHater(true);
+
+        katie2.heDidIt();
+        katie2.printReasons();
+
     }
-}
+} 
---
+++ WilliamHater.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class WilliamHater extends Hater{
+    
+    public WilliamHater(boolean isHating) {
+        super("William", isHating);
+    }
+
+    public void heDidIt() {
+        ArrayList<String> reasons = getReasons();
+        reasons.add("He made a stupid comment");
+    }
+}
