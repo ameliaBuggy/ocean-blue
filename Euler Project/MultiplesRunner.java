1b5050fa6cc1473bf76c2266fa06658fc0212471
ameliaBuggy <amelia.chiasson@gmail.com>
2025-09-04 12:14:06 -0400

    fibonacci numbers

--- Euler Project/MultiplesRunner.java
+++ Euler Project/MultiplesRunner.java
@@ -1,10 +1,16 @@
 public class MultiplesRunner {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
+        int current = 1;
+        int previous = 0;
+        int next = 0;
+        while (current < 4000000) {
+            if (current % 2 == 0) {
+                sum += current;
             }
+            next = current + previous;
+            previous = current;
+            current = next;
         }
         System.out.println(sum);
     }
