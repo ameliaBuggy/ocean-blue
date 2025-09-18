fb0d8d58152c11091c55bb23fa5025c5e26a9921
ameliaBuggy <amelia.chiasson@gmail.com>
2025-09-18 12:31:37 -0400

    weird/not weird

--- Euler Project/MultiplesRunner.java
+++ Euler Project/MultiplesRunner.java
@@ -1,21 +1,12 @@
 public class MultiplesRunner {
-    public static boolean isPrime(int n) {
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
+    public static void main(String[] args) {
+        for (int num = 1; num <= 100; num++) {
+            System.out.print(num);
+            if (num % 2 != 0 || (num >= 6 && num <= 20)) {
+                System.out.println(" Weird");
+            } else {
+                System.out.println(" Not Weird");
             }
         }
-        return true;
-    }
-    public static void main(String[] args) {
-        int count = 7;
-        int num = 13;
-        while (count <= 10001) {
-            num++;
-            if (isPrime(num)) {
-                count++;
-            } 
-        }
-        System.out.println(num);
     }
 }
