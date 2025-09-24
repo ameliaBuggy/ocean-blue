b4aa9a7049d86ff2cb3d5a3ae52378b5f6a7c126
ameliaBuggy <amelia.chiasson@gmail.com>
2025-09-24 12:36:40 -0400

    sum of primes under 2 million

--- Euler Project/MultiplesRunner.java
+++ Euler Project/MultiplesRunner.java
@@ -1,12 +1,18 @@
 public class MultiplesRunner {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        for (int num = 1; num <= 100; num++) {
-            System.out.print(num);
-            if (num % 2 != 0 || (num >= 6 && num <= 20)) {
-                System.out.println(" Weird");
-            } else {
-                System.out.println(" Not Weird");
+        int sum = 3;
+        for (int i = 3; i < 2000000; i+=2) {
+            if (isPrime(i)) {
+                sum += i;
             }
         }
+        System.out.println(sum);
     }
 }
