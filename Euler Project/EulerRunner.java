b7204a27764e29501c60d5c67308676314d51355
ameliaBuggy <amelia.chiasson@gmail.com>
2025-10-16 12:29:49 -0400

    palindrome number and binary sum

--- Euler Project/EulerRunner.java
+++ Euler Project/EulerRunner.java
@@ -1,18 +1,23 @@
 public class EulerRunner {
-    public static boolean isPrime(int n) {
-        if (n <= 1) return true;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+    public static boolean isPalindrome(String number){
+        for (int i = 0; i < number.length()/2; i++) {
+            if (number.charAt(i) != number.charAt(number.length() - 1 - i)) {
+                return false;
+            }
         }
         return true;
     }
     public static void main(String[] args) {
-        int sum = 3;
-        for (int i = 3; i < 2000000; i+=2) {
-            if (isPrime(i)) {
+        Integer sum = 0;
+        for (Integer i = 0; i < 1000000; i++) {
+            String number = i.toString();
+            String binary = Integer.toBinaryString(i);
+            if (isPalindrome(number) && isPalindrome(binary)) {
+                System.out.println("Number: " + number);
+                System.out.println("Binary " + binary);
                 sum += i;
             }
         }
-        System.out.println(sum);
+        System.out.println("Sum: " + sum);
     }
 }
