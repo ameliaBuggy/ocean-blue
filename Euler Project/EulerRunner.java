20a981d6062fded3c4dacbcad41b547e7ba43e2d
ameliaBuggy <amelia.chiasson@gmail.com>
2025-10-21 12:11:29 -0400

    clear file and new euler project

--- Euler Project/EulerRunner.java
+++ Euler Project/EulerRunner.java
@@ -1,23 +1,11 @@
 public class EulerRunner {
-    public static boolean isPalindrome(String number){
-        for (int i = 0; i < number.length()/2; i++) {
-            if (number.charAt(i) != number.charAt(number.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
     public static void main(String[] args) {
-        Integer sum = 0;
-        for (Integer i = 0; i < 1000000; i++) {
-            String number = i.toString();
-            String binary = Integer.toBinaryString(i);
-            if (isPalindrome(number) && isPalindrome(binary)) {
-                System.out.println("Number: " + number);
-                System.out.println("Binary " + binary);
-                sum += i;
-            }
+        int sum = 0;
+        for(int i = 0; i <= 1000; i++) {
+            sum += i**i;
         }
-        System.out.println("Sum: " + sum);
+
+        sum = sum.toString().substring(-10,-1);
+        System.out.println(sum);
     }
 }
