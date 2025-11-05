e2e8b641f3a2b24ecb661af5c65311091e831027
ameliaBuggy <amelia.chiasson@gmail.com>
2025-11-04 19:54:49 -0500

    fixed? + ruby

---
+++ EulerRunner.java
@@ -0,0 +1,5 @@
+public class EulerRunner {
+    public static void main(String[] args) {
+        System.out.println("Running Euler Problems...");
+    }
+}
---
+++ euler_runner.rb
@@ -0,0 +1,5 @@
+def test
+  puts "hello world"
+end
+
+test
